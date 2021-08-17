8b0e08b54947e89ed7896480724ca05755122903
Anastasya <bepin@yandex.ru>
2021-08-17 16:48:35 +0400

    refactoring

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
     public function index() 
     {
         $posts = Post::paginate(config('app.posts'));
-               
+        // var_dump($posts);
         return view('index', ['posts' => $posts]);
     }
 }
